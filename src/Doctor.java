c96ffa2514560f2ff8e9782c09e25014c8d8801c
fostp4040 <fostp4040@HRH-IC0027333.wrdsb.ca>
2016-10-31 12:25:04 -0400

    made the Dalek game

--- src/Doctor.java
+++ src/Doctor.java
@@ -11,6 +11,7 @@ public class Doctor {
     private int row;
     private int col;
     private boolean captured;
+    private boolean win;
     private int rand= (int)(Math.random()*12);
     public Doctor(int theRow, int theCol){
         this.row=theRow;
@@ -18,33 +19,33 @@ public class Doctor {
     }
     
     public void move(int newRow, int newCol){
-        if(row-newRow==1&&captured==false){
+        if(row-newRow==1&&captured==false&&win==false){
             //Doctor moves to selected space if it's right next to where the Doctor is
             row--;
-        }else if(row-newRow==-1&&captured==false){
+        }else if(row-newRow==-1&&captured==false&&win==false){
             //Doctor moves to selected space if it's right next to where the Doctor is
             row++;
-        }else if(row-newRow>=2||row-newRow<=-2&&captured==false){
+        }else if(row-newRow>=2||row-newRow<=-2&&captured==false&&win==false){
             //Doctor use the TARDIS to teleport to random space if it's not next to where the Doctor is
             row=rand;
-        }else if(row-newRow==0&&captured==false){
+        }else if(row-newRow==0&&captured==false&&win==false){
             //Doctor stays put if selected space is where the doctor is currently
             row=row;
         }
-        if(col-newCol==1&&captured==false){
+        if(col-newCol==1&&captured==false&&win==false){
             //Doctor moves to selected space if it's right next to where the Doctor is
             col--;
-        }else if(col-newCol==-1&&captured==false){
+        }else if(col-newCol==-1&&captured==false&&win==false){
             //Doctor moves to selected space if it's right next to where the Doctor is
             col++;
-        }else if(col-newCol>=2||col+newCol<=2&&captured==false){
+        }else if(col-newCol>=2||col+newCol<=2&&captured==false&&win==false){
             //Doctor use the TARDIS to teleport to random space if it's not next to where the Doctor is
             col=rand;
-        }else if(col-newCol==0&&captured==false){
+        }else if(col-newCol==0&&captured==false&&win==false){
             //Doctor stays put if selected space is where the doctor is currently
             col=col;
         } 
-        if(row-newRow==0&&col-newCol==0&&captured==false){
+        if(row-newRow==0&&col-newCol==0&&captured==false&&win==false){
             //Doctor stays put if selected space is where the doctor is currently
             row=row;
             col=col;
@@ -61,8 +62,22 @@ public class Doctor {
         row=row;
         col=col;
     }
+    public void win(){
+        win=true;
+        row=row;
+        col=col;
+    }
     public boolean gotCaptured(){
         if (captured==true){
+            row=row;
+            col=col;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public boolean won(){
+        if (win==true){
             row=row;
             col=col;
             return true;
